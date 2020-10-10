19e27c1beb97e156cf2dd244a4dd3c284c4bf86d
Thunderstormonster <nastiy0205@gmail.com>
2020-10-10 15:38:46 +0300

    Добавьте файлы проекта.

---
+++ Lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bl = true;
+            Console.WriteLine(bl);
+            byte bt = 236;
+            Console.WriteLine(bt);
+            sbyte sbt = -3;
+            Console.WriteLine(sbt);
+            char ch = 'a';
+            Console.WriteLine(ch);
+            decimal de = 3000.5m;
+            Console.WriteLine(de);
+            double dou = 1.5d;
+            Console.WriteLine(dou);
+            float fl = 2.0094f;
+            Console.WriteLine(fl);
+            int i = 3;
+            Console.WriteLine(i);
+            uint u= 4294967295;
+            Console.WriteLine(u);
+            long l = 9223372036854775807;
+            Console.WriteLine(l);
+            ulong ul = 18446744073709551615;
+            Console.WriteLine(ul);
+            short sh = 32767;
+            Console.WriteLine(sh);
+            ushort ush = 65535;
+            Console.WriteLine(ush);
+
+        }
+    }
+}
